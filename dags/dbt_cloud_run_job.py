f8d7d5842d849cf267e1d6466550179b29edc5b2
João Torresini <104875177+Torresini@users.noreply.github.com>
2025-02-25 18:19:45 -0300

    Add files via upload

---
+++ dags/dbt_cloud_run_job.py
@@ -0,0 +1,28 @@
+
+from datetime import datetime
+
+from airflow.models import DAG
+from airflow.providers.dbt.cloud.operators.dbt import DbtCloudRunJobOperator
+
+
+DBT_CLOUD_CONN_ID = "dbt_cloud_run_job"
+ACCOUNT_ID = "70471823433105"
+JOB_ID = "70471823431448"
+
+with DAG(
+    dag_id="dbt_cloud_run_job",
+    default_args={"dbt_cloud_conn_id": DBT_CLOUD_CONN_ID, "account_id": ACCOUNT_ID},
+    start_date=datetime(2021, 1, 1),
+    schedule_interval="0 11 * * *",
+    catchup=False,
+) as dag:
+
+    trigger_dbt_cloud_job_run = DbtCloudRunJobOperator(
+        task_id="trigger_dbt_cloud_job_run",
+        job_id=JOB_ID,
+        check_interval=10,
+        timeout=300,
+        retry_from_failure=True,
+    )
+
+    trigger_dbt_cloud_job_run
